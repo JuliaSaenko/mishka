562142145218e7ce6340b2fe2995e37c3fdd97a5
JuliaSaenko <julia.saenko94@gmail.com>
2018-10-16 09:59:33 +0300

    Адаптивные декоративные элементы 1.0

---
+++ source/js/script.js
@@ -0,0 +1,30 @@
+//Меню
+  var navMain = document.querySelector(".main-nav");
+  var navToggle = document.querySelector(".main-nav__toggle");
+
+  navMain.classList.remove("main-nav--nojs");
+
+  navToggle.addEventListener("click", function() {
+    if (navMain.classList.contains("main-nav--closed")) {
+      navMain.classList.remove("main-nav--closed");
+      navMain.classList.add("main-nav--opened");
+    } else {
+      navMain.classList.add("main-nav--closed");
+      navMain.classList.remove("main-nav--opened");
+    }
+  });
+
+//Модальное окно
+var button = document.querySelectorAll(".button");
+var modal = document.querySelector(".modal");
+var close = modal.querySelector(".choose-size__button");
+
+button.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  modal.classList.add("modal--show");
+  });
+
+  close.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    modal.classList.remove("modal--show");
+  });
