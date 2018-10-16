cb34cc0e4127ca4d25781f1124f3188fa1c5ce84
JuliaSaenko <julia.saenko94@gmail.com>
2018-10-16 20:43:09 +0300

    Исправила JS

--- source/js/script.js
+++ source/js/script.js
@@ -1,3 +1,4 @@
+"use strict";
 //Меню
   var navMain = document.querySelector(".main-nav");
   var navToggle = document.querySelector(".main-nav__toggle");
@@ -19,10 +20,11 @@ var button = document.querySelectorAll(".button");
 var modal = document.querySelector(".modal");
 var close = modal.querySelector(".choose-size__button");
 
-button.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  modal.classList.add("modal--show");
-  });
+for (var i = 0; i < button.length; i++) {
+  button[i].addEventListener("click", function (evt) {
+    evt.preventDefault();
+    modal.classList.add("modal--show");
+});
 
   close.addEventListener("click", function (evt) {
     evt.preventDefault();
